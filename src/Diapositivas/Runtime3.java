18a17cf13727029722cf0cbacff5a053ba036991
mañana <maana@192.168.17.250>
2015-09-29 10:37:59 +0200

    terminado ejercicio

--- src/Diapositivas/Runtime3.java
+++ src/Diapositivas/Runtime3.java
@@ -21,22 +21,22 @@ public class Runtime3 {
 		
 		try {
 			proceso = runtime.exec("netstat -ano");
-			proceso2 = runtime.exec("ping");
+			proceso2 = runtime.exec("find \"LISTEN\"");
 			in = new BufferedReader(new InputStreamReader(proceso.getInputStream()));
-			//proceso.waitFor();
-			String valores = null;
+			proceso.waitFor();
+			String valores;String lista="";
 			while((valores=in.readLine())!=null){
-				valores=in.readLine();
+				lista+=valores+"\n";
 			}
 			out = new BufferedWriter(new OutputStreamWriter(proceso2.getOutputStream()));
-			//proceso2.waitFor();
-			out.write(valores);
+			out.write(lista);
 			out.flush();
 			out.close();
 			in2 = new BufferedReader(new InputStreamReader(proceso2.getInputStream()));
-			while((valores=in.readLine())!=null){
+			while((valores=in2.readLine())!=null){
 				System.out.println(valores);
 			}
+			
 			
 			
 		} catch (IOException e) {
